d9c106ba4d36a735cfa52764657f838f0790ee97
Yure Matias <ymatias2000@gmail.com>
2019-06-08 22:29:33 -0300

    Add files via upload

--- QuestionSystem.cpp
+++ QuestionSystem.cpp
@@ -10,23 +10,24 @@
 struct questao
 {
 	char texto[300];
-	char resposta[200];
+	char resposta[300];
 	char tema[50];
 	char dominio[50];	
 	int dificulade;
 	int id;
-}questoes[100];
+}questoes;
 
-void fazerQuery(unsigned char cont, char* query);
 void gotoxy(int x, int y);
 unsigned char menu(char menu[40]);
 void menuBusca(unsigned char op);
 void menuPrincipal(unsigned char op);
-int cadastroQuestoes(unsigned char cont, PGconn *conn);
+void cadastrarQuestao(PGconn *conn);
+void cadastrarDominio(PGconn *conn);
+void cadastrarTema(PGconn *conn);
 void mostrarOpcoes(unsigned char op);
+void telaCadastro(PGconn *conn);
 
-static void
-exit_nicely(PGconn *conn)
+static void exit_nicely(PGconn *conn)
 {
     PQfinish(conn);
     system("pause");
@@ -52,7 +53,6 @@ int main()
         exit_nicely(conn);
     }
 	    
-	int cont = 0;
 	setlocale(LC_ALL, "Portuguese");
 	system("color 0F");
 	unsigned char op, opBusca;
@@ -62,7 +62,7 @@ int main()
 		switch(op)
 		{
 			case 1:
-				cont = cadastroQuestoes(cont, conn);
+				telaCadastro(conn);
 				break;
 			case 2:
 				opBusca = menu("busca");
@@ -75,6 +75,7 @@ int main()
 	return 0;
 }
 
+// funcao que mostra as opcoes do menu principal
 void menuPrincipal(unsigned char op)
 {
 	printf(CIANO"\t========= SISTEMA DE QUESTÕES ========="CINZA);
@@ -83,7 +84,7 @@ void menuPrincipal(unsigned char op)
 	(op == 3) ? printf(""PRETO FUNDOBRANCO"\n\n\t Sair "CINZA FUNDOPRETO " <==\n\n") : printf("\n\n\t Sair\n\n");
 	printf(CIANO"\t=======================================\n"CINZA);
 }
-
+// funcao que mostra as opcoes do menu de busca
 void menuBusca(unsigned char op)
 {
 	printf(CIANO"\t========= BUSCA DE QUESTÕES ========="CINZA);
@@ -95,6 +96,7 @@ void menuBusca(unsigned char op)
  	printf(CIANO"\n\n\t=======================================\n"CINZA);
 }
 
+// funcao que faz o controle do menu com as setas 
 unsigned char menu(char* menu)
 {
 	unsigned char limite;
@@ -143,68 +145,127 @@ unsigned char menu(char* menu)
 						if(op < limite)
 							op++;			
 				}
-				break;			// sai do laço se o usuario teclar algo 
+				break;	// sai do laço se o usuario teclar algo, isto para mostrar o menu com as opcoes ja alteradas
 			}
 	}while(tecla != 13); // 13 eh o enter da tabela ascii
 	return op;
 }
 
-int cadastroQuestoes(unsigned char cont, PGconn *conn)
+// funcao da tela de cadastro, onde o usuario digita todas as entradas 
+void telaCadastro(PGconn *conn)
 {	
 	system("cls");
-	gotoxy(5,26);
+	gotoxy(5,23);
 	printf(CIANO"\t==============================="CINZA);
 	gotoxy(5,1);
 	printf(CIANO"\t===== CADASTRO DE QUESTÕES ====="CINZA);
 	printf(AMARELO"\n\n\tEscreva a questão:\n\n\t"CINZA);
 	setbuf(stdin,NULL);
-	fgets(questoes[cont].texto,300,stdin);
+	fgets(questoes.texto,300,stdin);
 	printf(AMARELO"\n\tEscreva a resposta da questão:\n\n\t"CINZA);
-	fgets(questoes[cont].resposta,50,stdin);
-	printf(AMARELO"\n\tInsira o tema da questão:\n\n\t"CINZA);
-	setbuf(stdin,NULL);
-	fgets(questoes[cont].tema,50,stdin);
+	fgets(questoes.resposta,300,stdin);
 	printf(AMARELO"\n\tInsira o dominio da questão\n\n\t"CINZA);
-	fgets(questoes[cont].dominio,50,stdin);
-	printf(AMARELO"\n\tInsira a dificuldade da questão: de 1 a 10\n\n\t"CINZA);
-	scanf("%d",&questoes[cont].dificulade);
-	printf(AMARELO"\n\tInsira o ID da questão:\n\n\t"CINZA);
-	scanf("%d",&questoes[cont].id);
-	
+	fgets(questoes.dominio,50,stdin);
+	printf(AMARELO"\n\tInsira o tema da questão:\n\n\t"CINZA);
+	fgets(questoes.tema,50,stdin);
+	printf(AMARELO"\n\tInsira a dificuldade da questão:\n\n\t"CINZA);
+	scanf("%d",&questoes.dificulade);
+	cadastrarDominio(conn);
+	cadastrarTema(conn);
+	cadastrarQuestao(conn);
+    printf("\n\t");
+	Sleep(1200);	
+}
+
+// prepara a query e insire o tema anteriormente digitado no banco de dados, 
+// esta funcao eh chamada dentro da funcao telaCadastro.
+void cadastrarTema(PGconn *conn)
+{
 	char query[500];
-	strcpy(query,"insert into question values('");
-	fazerQuery(cont, query);
+	strcpy(query,"INSERT INTO Theme VALUES('");
+	strcat(query,questoes.tema);
+	strcat(query,"')");
 	PGresult *res = PQexec(conn, query);
 		/* handle the response */
 	printf("\n");	
 	switch (PQresultStatus(res)) 
 	{
 		case PGRES_EMPTY_QUERY: 
-		case PGRES_COMMAND_OK: printf("Cadastro realizado com sucesso...\n"); break;
+		case PGRES_COMMAND_OK: break;
 		case PGRES_TUPLES_OK:  break;
-		case PGRES_BAD_RESPONSE: printf("[ERROR] Bad Response"); break;
+		case PGRES_BAD_RESPONSE: printf(VERMELHO"\t[ERROR] Bad Response\n"CINZA); system("pause"); break;
 		case PGRES_NONFATAL_ERROR: 
 		case PGRES_FATAL_ERROR: printf(PQresultErrorMessage(res)); break;
-		default: printf("Unexpected Response");
+		default: printf(VERMELHO"\tUnexpected Response\n"CINZA); system("pause");
 	}	
     PQclear(res);
-    printf("\n");
-	system("pause");
-	return ++cont;	
 }
 
-void fazerQuery(unsigned char cont, char* query)
-{ 
-	strcat(query,questoes[cont].texto);
-	strcat(query,"','");
-	strcat(query,questoes[cont].resposta);
-	strcat(query,"','");
-	char a[10]; 
-	sprintf(a,"%d",questoes[cont].id);
-	strcat(query ,a);
+// prepara a query e insire o dominio anteriormente digitado no banco de dados, 
+// esta funcao eh chamada dentro da funcao telaCadastro.
+void cadastrarDominio(PGconn *conn)
+{
+	char query[500];
+	strcpy(query,"INSERT INTO Domain VALUES('");
+	strcat(query,questoes.dominio);
 	strcat(query,"')");
+	PGresult *res = PQexec(conn, query);
+		/* handle the response */
+	printf("\n");	
+	switch (PQresultStatus(res)) 
+	{
+		case PGRES_EMPTY_QUERY: 
+		case PGRES_COMMAND_OK: break;
+		case PGRES_TUPLES_OK:  break;
+		case PGRES_BAD_RESPONSE: printf(VERMELHO"\t[ERROR] Bad Response\n"CINZA); system("pause"); break;
+		case PGRES_NONFATAL_ERROR: 
+		case PGRES_FATAL_ERROR: printf(PQresultErrorMessage(res)); break;
+		default: printf(VERMELHO"\tUnexpected Response\n"CINZA); system("pause");
+	}	
+    PQclear(res);
 }
 
+// prepara a query e insire a questao no banco de dados, 
+// esta funcao eh chamada dentro da funcao telaCadastro.
+void cadastrarQuestao(PGconn *conn)
+{ 
+	char query[500];
+	strcpy(query,"INSERT INTO Question VALUES(");
+	strcat(query, "DEFAULT,'");		    	// colocar DEFAULT para ocorrer o auto-incremento no banco 
+	strcat(query,questoes.texto);
+	strcat(query,"','");
+	strcat(query,questoes.resposta);
+	strcat(query,"','");
+	strcat(query,questoes.dominio);
+	strcat(query,"','");
+	strcat(query,questoes.tema);
+	strcat(query,"',");
+	
+	// eh necessario converter a dificuldade para char 
+	// para que o valor seja colocado na string
+	char dificulty[15];
+	sprintf(dificulty, "%d", questoes.dificulade);
+	// fim da conversao
+	
+	strcat(query,dificulty);
+	strcat(query,")");
+	PGresult *res = PQexec(conn, query);
+		/* handle the response */
+	printf("\n");	
+	switch (PQresultStatus(res)) 
+	{
+		case PGRES_EMPTY_QUERY: 
+		case PGRES_COMMAND_OK: printf(CIANO"\tCadastro realizado com sucesso...\n"CINZA); break;
+		case PGRES_TUPLES_OK:  break;
+		case PGRES_BAD_RESPONSE: printf(VERMELHO"\t[ERROR] Bad Response\n"CINZA); system("pause"); break;
+		case PGRES_NONFATAL_ERROR: 
+		case PGRES_FATAL_ERROR: printf(PQresultErrorMessage(res)); break;
+		default: printf(VERMELHO"\tUnexpected Response\n"CINZA); system("pause");
+	}	
+    PQclear(res);
+}
+
+//	funcao que mexe o cursor do terminal para as cordenadas x,y  
 void gotoxy(int x, int y)
 {
     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),(COORD){x-1,y-1});
